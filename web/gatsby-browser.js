9b4c1fc61e726d216dd8329ce55973def15e4e9f
Ed Wilson <ed@gendall.co.uk>
2023-01-17 13:15:09 +0000

    blakcer font

--- web/gatsby-browser.js
+++ web/gatsby-browser.js
@@ -1,5 +1,8 @@
-import "bootstrap/dist/js/bootstrap.min.js";
-import "@popperjs/core/dist/umd/popper.min.js";
+import "bootstrap/dist/js/bootstrap.min.js"
+import "@popperjs/core/dist/umd/popper.min.js"
 
 import "@fontsource/montserrat/400.css"
 import "@fontsource/montserrat/700.css"
+import "./src/fonts/blackerDisplay/style.css"
+
+export { wrapRootElement } from "./wrapRootElement"
