e8ac073c6e2a93fce592aac1df8fe6ffc99cfeb3
Ed Wilson <ed@gendall.co.uk>
2023-01-26 22:29:27 +0000

    filter on selected color

--- web/src/components/filter.js
+++ web/src/components/filter.js
@@ -9,6 +9,7 @@ import CloseIcon from '@mui/icons-material/Close';
 export const Filter = ({allPlace, filterPlaces, setFilterPlaces}) => {
 
   const [open, setOpen] = useState("hidden");
+  const [selectedFilters, setSelectedFilters] = useState(null)
 
 
   const doFilterPlaces = (taxonomy, e) => {
@@ -17,7 +18,9 @@ export const Filter = ({allPlace, filterPlaces, setFilterPlaces}) => {
   }
 
   const addToFilter = (taxonomy) => {
-   
+
+    setSelectedFilters(taxonomy)
+    
     const filtered = allPlace.filter(e => doFilterPlaces(taxonomy, e));
     console.log('filterd', filtered)
     setFilterPlaces([...filtered])
@@ -56,17 +59,17 @@ export const Filter = ({allPlace, filterPlaces, setFilterPlaces}) => {
     <Container maxWidth="lg" sx={{mt: 9}}>
 
         <motion.div variants={filterBox} initial="hidden" animate={open}>
-          <Box sx={{p: {xs: 9}, backgroundColor: "primary.main"}} display="flex">
+          <Box sx={{flexDirection: {xs: 'column', md: 'row'}, p: {xs: 4, md: 9}, backgroundColor: "primary.main"}} display="flex">
 
-            <Box display="flex" flexWrap="wrap" justifyContent="space-between" sx={{columnGap: 3, rowGap: 3, flexGrow: 1}}> 
+            <Box display="flex" flexWrap="wrap" sx={{justifyContent: {xs: 'space-around', md: 'space-between'}, columnGap: 3, rowGap: 3, flexGrow: 1, order: {xs: 2, md: 1}}}> 
               {data.allSanityCategories.nodes.map((taxonomy, i) => {
                 return(
-                  <Button key={taxonomy.name} onClick={e => addToFilter(taxonomy.name) }variant="contained" disableElevation sx={{borderRadius: 0, color:"white", backgroundColor: "primary.mid" }}>{taxonomy.name}</Button>
+                  <Button key={taxonomy.name} onClick={e => addToFilter(taxonomy.name)} variant="contained" disableElevation sx={{borderRadius: 0, color:"white", backgroundColor: selectedFilters === taxonomy.name ? "primary.dark" : "primary.mid" }}>{taxonomy.name}</Button>
                 )
               })}
             </Box>
 
-            <Box display="flex" alignItems="center" justifyContent="flex-end" sx={{flexGrow: 1}}>
+            <Box display="flex" alignItems="center" justifyContent="flex-end" sx={{flexGrow: 1, order: {xs: 1, md: 2}}}>
               <IconButton aria-label="close filter" color="white" onClick={e => handleClick("hidden")}>
                 <CloseIcon />
               </IconButton>
@@ -76,7 +79,8 @@ export const Filter = ({allPlace, filterPlaces, setFilterPlaces}) => {
         </motion.div>
 
       {open === 'hidden' && <Box display="flex" justifyContent="center">
-        <Button onClick={e => handleClick("visible")} variant="contained" startIcon={<AppsIcon />} sx={{ display: 'flex'}}>Filter</Button>
+        <Button onClick={e => handleClick("visible")} variant="contained" startIcon={<AppsIcon />} sx={{ display: 'flex', mx: 1}}>Filter</Button>
+        <Button onClick={e => setFilterPlaces([...allPlace])} variant="text" sx={{ mx: 1}}>Reset</Button>
       </Box>}
 
     </Container>
