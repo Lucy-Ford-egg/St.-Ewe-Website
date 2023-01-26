d8d7d80500ffb27f566dba852211b373fdc86b9b
Ed Wilson <ed@gendall.co.uk>
2023-01-26 13:34:49 +0000

    widths edit

--- studio/schemas/widths.js
+++ studio/schemas/widths.js
@@ -4,16 +4,36 @@ export default [
     value: "100%"
   },
   {
-    title: "75%",
-    value: "75%"
+    title: "90%",
+    value: "90%"
+  },
+  {
+    title: "80%",
+    value: "80%"
+  },
+  {
+    title: "70%",
+    value: "70%"
+  },
+  {
+    title: "60%",
+    value: "60%"
   },
   {
     title: "50%",
     value: "50%"
   },
   {
-    title: "25%",
-    value: "25%"
+    title: "40%",
+    value: "40%"
+  },
+  {
+    title: "30%",
+    value: "30%"
+  },
+  {
+    title: "20%",
+    value: "20%"
   }
 ]
 
