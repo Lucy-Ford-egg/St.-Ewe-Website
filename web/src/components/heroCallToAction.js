ee736aa5b238d7ed5c380e89213ce41e04cc1cc5
Ed Wilson <ed@gendall.co.uk>
2023-02-06 12:55:31 +0000

    grid col tweak

--- web/src/components/heroCallToAction.js
+++ web/src/components/heroCallToAction.js
@@ -28,7 +28,7 @@ export const HeroCallToAction = ({ title, text, image, linkGroup }) => {
 
   return (
     <Container maxWidth="false" sx={{ px: {xs: 0}, py: { xs: 2, md: 6 }, display: 'grid', gridTemplateColumns: '5% auto 5%', gridTemplateRows: {xs: '30px 1fr 1fr 1fr 30px', md: 'repeat(6, 1fr)'} }}>
-      <Container maxWidth="sm" sx={{ backgroundColor: 'primary.main', gridColumn: {xs: '2/2', md: '1/3'}, gridRow: {xs: '2/5', md: '2/6'}, position: 'relative', zIndex: 1, pt: {xs: 6, md: 6}, pb: {xs: 6, md: 3}, px: {xs: 0, md: 10}, mb: {xs: 0, md: -8} }}>
+      <Container maxWidth="sm" sx={{ backgroundColor: 'primary.main', gridColumn: {xs: '2/2', md: '1/4'}, gridRow: {xs: '2/5', md: '2/6'}, position: 'relative', zIndex: 1, pt: {xs: 6, md: 6}, pb: {xs: 6, md: 3}, px: {xs: 0, md: 10}, mb: {xs: 0, md: -8} }}>
 
         <Typography align="center" sx={{textAlign: 'center', my: { xs: 5 } }} variant='h2'>{title}</Typography>
         <Typography align="center" sx={{textAlign: 'center', my: { xs: 5 }, maxWidth: 'max-content' }} variant='body1' color='white.main' dangerouslySetInnerHTML={{__html: text}}/>
