003f8737b4ff1a6b30d811756e86ed55a9c66685
Ed Wilson <ed@gendall.co.uk>
2023-02-07 15:37:35 +0000

    more checks

--- web/src/utils/buttonLink.js
+++ web/src/utils/buttonLink.js
@@ -8,11 +8,11 @@ const ButtonLink = ({linkGroup, buttonType = "contained", color = "primary"}) =>
 
   if(linkGroup && linkGroup.internalLinkGroup){
     linkType = <Button variant={buttonType} color={color} component={Link} to={linkGroup?.internalLinkGroup?.reference?.slug.current}  aria-label={`Link to ${linkGroup?.internalLinkGroup?.label}`}>
-          {linkGroup?.internalLinkGroup?.label}
+          {linkGroup?.internalLinkGroup && linkGroup?.internalLinkGroup?.label}
         </Button>
   }
   if(linkGroup && linkGroup.externalLinkGroup){
-    linkType = <Button variant={buttonType} color={color} className="" href={linkGroup?.externalLinkGroup?.href} aria-label={`Link to ${linkGroup?.externalLinkGroup?.label}`}>{linkGroup?.externalLinkGroup?.label}</Button>
+    linkType = <Button variant={buttonType} color={color} className="" href={linkGroup?.externalLinkGroup?.href} aria-label={`Link to ${linkGroup?.externalLinkGroup?.label}`}>{linkGroup?.externalLinkGroup && linkGroup?.externalLinkGroup?.label}</Button>
   }
   else{
     return false
