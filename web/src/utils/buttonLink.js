17d061af11e90e6c5d05e8487b139f224fa7c582
Ed Wilson <ed@gendall.co.uk>
2023-02-07 14:33:44 +0000

    null checking

--- web/src/utils/buttonLink.js
+++ web/src/utils/buttonLink.js
@@ -7,12 +7,12 @@ const ButtonLink = ({linkGroup, buttonType = "contained", color = "primary"}) =>
   let linkType = '';
 
   if(linkGroup?.internalLinkGroup !== null){
-    linkType = <Button variant={buttonType} color={color} component={Link} to={linkGroup?.internalLinkGroup.reference?.slug.current}  aria-label={`Link to ${linkGroup?.internalLinkGroup.label}`}>
-          {linkGroup?.internalLinkGroup.label}
+    linkType = <Button variant={buttonType} color={color} component={Link} to={linkGroup?.internalLinkGroup?.reference?.slug.current}  aria-label={`Link to ${linkGroup?.internalLinkGroup?.label}`}>
+          {linkGroup?.internalLinkGroup?.label}
         </Button>
   }
   else{
-    linkType = <Button variant={buttonType} color={color} className="" href={linkGroup.externalLinkGroup?.href} aria-label={`Link to ${linkGroup.externalLinkGroup?.label}`}>{linkGroup.externalLinkGroup?.label}</Button>
+    linkType = <Button variant={buttonType} color={color} className="" href={linkGroup?.externalLinkGroup?.href} aria-label={`Link to ${linkGroup?.externalLinkGroup?.label}`}>{linkGroup?.externalLinkGroup?.label}</Button>
   }
   return(linkType)
 }
