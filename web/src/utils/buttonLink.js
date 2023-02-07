563c6f156c9504e295fd9cd9c4a84d6ffd69fc86
Ed Wilson <ed@gendall.co.uk>
2023-02-07 15:35:08 +0000

    null checking the internal and external just in case no link is added

--- web/src/utils/buttonLink.js
+++ web/src/utils/buttonLink.js
@@ -6,14 +6,18 @@ const ButtonLink = ({linkGroup, buttonType = "contained", color = "primary"}) =>
 
   let linkType = '';
 
-  if(linkGroup?.internalLinkGroup !== null){
+  if(linkGroup && linkGroup.internalLinkGroup){
     linkType = <Button variant={buttonType} color={color} component={Link} to={linkGroup?.internalLinkGroup?.reference?.slug.current}  aria-label={`Link to ${linkGroup?.internalLinkGroup?.label}`}>
           {linkGroup?.internalLinkGroup?.label}
         </Button>
   }
-  else{
+  if(linkGroup && linkGroup.externalLinkGroup){
     linkType = <Button variant={buttonType} color={color} className="" href={linkGroup?.externalLinkGroup?.href} aria-label={`Link to ${linkGroup?.externalLinkGroup?.label}`}>{linkGroup?.externalLinkGroup?.label}</Button>
   }
+  else{
+    return false
+  }
+
   return(linkType)
 }
 
