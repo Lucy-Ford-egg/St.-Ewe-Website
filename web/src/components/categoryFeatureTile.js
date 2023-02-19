2c1eef6a3f6969187b2709c4d97b06d04177679d
Ed Wilson <ed@gendall.co.uk>
2023-02-19 21:49:32 +0000

    media queried cat tiles

--- web/src/components/categoryFeatureTile.js
+++ web/src/components/categoryFeatureTile.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, forwardRef } from 'react'
 import { graphql } from "gatsby"
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import { Grid, Box, Typography } from '@mui/material'
@@ -11,29 +11,78 @@ export const CategoryFeatureTile = ({ node, i }) => {
 
   const overlay = {
     hidden: {
-      y: 240,
-      
+      y: '74%',
+      transition: {
+        type: "spring",
+        bounce: 0,
+        damping: 20,
+      }
+
     },
-    visible:{
+    visible: {
       y: 0,
-     
+      transition: {
+        type: "spring",
+        bounce: 0,
+        damping: 20,
+      }
+    }
+  }
+
+  const Inner = forwardRef((props, ref ) => (
+    <Box ref={ref} {...props}>
+      <Typography sx={{ mb: { xs: 4 } }} align="center" variant="h3">{node.name}</Typography>
+      <motion.div variants={innerText}>
+        <Typography sx={{fontSize: {xs: 12, sm: 16, md: 12, lg: 16}}} align="center" color="white.main">{node.excerpt}</Typography>
+      </motion.div>
+    </Box>
+  ))
+  const MotionInner = motion(Inner, { forwardMotionProps: true })
+
+  const inner = {
+    hidden: {
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+      transition: {
+        type: "spring",
+        bounce: 0,
+        delayChildren: 0.2
+      }
+    },
+    visible: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      transition: {
+        type: "spring",
+        bounce: 0,
+        delayChildren: 0.2
+      }
+    }
+  }
+
+  const innerText = {
+    hidden: { 
+      opacity: 0 
+    },
+    visible: { 
+      opacity: 1 
     }
   }
 
   return (
-    <Grid onMouseOver={() => setShowOverlay(true)} onMouseLeave={() => setShowOverlay(false)} key={`column-${i}`} item xs={12} md={4} sx={{
-      overflow: 'hidden', height: 374, position: 'relative', display: 'grid', gridTemplateColumns: '1fr', gridTemplateRows: '1fr', '&:hover': {
+    <Grid onMouseOver={() => setShowOverlay(true)} onMouseLeave={() => setShowOverlay(false)} key={`column-${i}`} item xs={12} sm={12} md={4} lg={4} sx={{
+      overflow: 'hidden',  minHeight: 527, maxHeight: 527, height: 'min-content', position: 'relative', display: 'grid', gridTemplateColumns: '1fr', gridTemplateRows: '1fr', '&:hover': {
         cursor: 'pointer'
       }
     }}>
       <Box sx={{ position: 'relative', zIndex: 0, gridColumn: '1/2', gridRow: '1/2' }}>
-        <GatsbyImage image={getImage(node.picture?.asset.gatsbyImageData)} />
+        <GatsbyImage style={{height: '100%',  minHeight: 527, maxHeight: 527}} image={getImage(node.picture?.asset.gatsbyImageData)} />
       </Box>
-      <motion.div initial="hidden" animate={showOverlay ? "visible" : "hidden"} variants={overlay} style={{zIndex: 1, position: 'relative', gridColumn: '1/2', gridRow: '1/2',}}>
-        <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', backgroundColor: clientTheme.palette.primary.main, p: { xs: 6 } }}>
-          <Typography sx={{ mb: { xs: 4 } }} align="center" variant="h3">{node.name}</Typography>
-          <Typography align="center" color="white.main">{node.excerpt}</Typography>
-        </Box>
+      <motion.div initial="hidden" animate={showOverlay ? "visible" : "hidden"} variants={overlay} style={{ zIndex: 1, position: 'relative', gridColumn: '1/2', gridRow: '1/2', }}>
+        <MotionInner initial="hidden" animate={showOverlay ? "visible" : "hidden"} variants={inner} sx={{
+          display: 'flex',
+          flexDirection: 'column', minHeight: 527, maxHeight: 527, height: '100%', backgroundColor: clientTheme.palette.primary.main, p: { xs: 6 }
+        }} />
       </motion.div>
     </Grid>
   )
@@ -49,8 +98,8 @@ export const query = graphql`
       picture {
         asset {
           gatsbyImageData(
-            width: 410,
-            height: 374
+           
+            height: 527
           )
         }
       }
