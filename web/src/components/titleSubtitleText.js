c5950c791a8f8c343c5e53a4589bc4e4ee99673f
Ed Wilson <ed@gendall.co.uk>
2023-02-20 20:14:22 +0000

    padding fix

--- web/src/components/titleSubtitleText.js
+++ web/src/components/titleSubtitleText.js
@@ -1,4 +1,4 @@
-import React, {useMemo} from "react"
+import React from "react"
 import { graphql } from "gatsby"
 import { Container, Typography, Box } from "@mui/material"
 import { AnimatedText } from "./animatedText"
@@ -7,9 +7,8 @@ import {ArchIcon} from "../components/archIcon"
 
 export const TitleSubtitleText = ({ title, subtitle, text, subtitlePosition, titleSize, titleWidth, adornment }) => {
 
-  const animatedTitle = useMemo(() => <AnimatedText subtitlePosition={subtitlePosition} text={title} titleSize={titleSize ? titleSize : "h2"} titleWidth={titleWidth ? titleWidth : "100%"} />, [])
   return (
-    <Container maxWidth={false} sx={{ py: { xs: 4, md: 9 } }}>
+    <Container maxWidth={false} sx={{ py: { xs: 4, md: 6 } }}>
       <Container maxWidth="md">
         {subtitlePosition === null && (
           <Typography
@@ -21,7 +20,7 @@ export const TitleSubtitleText = ({ title, subtitle, text, subtitlePosition, tit
             {subtitle}
           </Typography>
         )}
-        {animatedTitle}
+        <AnimatedText subtitlePosition={subtitlePosition} text={title} titleSize={titleSize ? titleSize : "h2"} titleWidth={titleWidth ? titleWidth : "100%"} />
         {subtitlePosition && (
           <Typography variant="subtitle2" color="primary.main" align="center" sx={{ pb: subtitlePosition !== null ? 8 : 8 }}>
             {subtitle}
@@ -34,7 +33,7 @@ export const TitleSubtitleText = ({ title, subtitle, text, subtitlePosition, tit
       </Container>
 
       {adornment &&
-        <Container maxWidth="sm" sx={{py: {xs: 8}}}>
+        <Container maxWidth="sm" sx={{py: {xs: 6}}}>
           <Box display="flex" justifyContent="center" alignItems="center">
             <Box display="span" sx={{ width: "85px", borderBottom: `1px solid ${clientTheme.palette.secondary.main}`, mx: {xs: 7} }}></Box>
             <ArchIcon />
