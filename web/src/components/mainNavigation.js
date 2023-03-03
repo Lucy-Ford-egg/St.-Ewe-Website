4f21d1409738b218e6c02b846cc7f4c4a788e1ea
Ed Wilson <ed@gendall.co.uk>
2023-03-03 14:58:50 +0000

    active menu item

--- web/src/components/mainNavigation.js
+++ web/src/components/mainNavigation.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby"
 import { styled } from '@mui/material/styles'
 import { Box } from "@mui/material"
 import { animationHover } from "../utils/animationHover"
+import clientTheme from "../gatsby-theme-material-ui-top-layout/theme"
 
 const StyledLink = styled(Link)(({ theme }) => ({
   color: 'inherit',
@@ -21,13 +22,13 @@ const MainNavigation = (props) => {
   return (
     <Box sx={{ flexGrow: 1, display: { xs: 'none', lg: 'flex' }, justifyContent: 'flex-end' }}>
 
-      {menu.sanityNavigation.items.map((menuItem) => {
+      {menu.sanityNavigation.items.map((menuItem, i) => {
         return (
           <Box
             key={menuItem.text}
             sx={{ my: 2, mx: { xs: 1, md: 1 }, px: { xs: 1, md: 1 }, color: "secondary.main", display: 'block', fontWeight: '500', textTransform: "unset" }}
           >
-            <StyledLink to={`/${menuItem.navigationItemUrl.internalLink.slug.current}`}>{menuItem.text}</StyledLink>
+            <StyledLink activeStyle={{ color: clientTheme.palette.primary.main }} to={`/${menuItem.navigationItemUrl.internalLink.slug.current}`}>{menuItem.text}</StyledLink>
           </Box>
         )
       })
