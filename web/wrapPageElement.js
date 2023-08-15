ef6bd3c8a69ab3525af677a5833655516abff4b8
Ed Wilson <ed@gendall.co.uk>
2023-08-15 15:40:49 +0100

    gtag in Script component

--- web/wrapPageElement.js
+++ web/wrapPageElement.js
@@ -1,4 +1,5 @@
 import React from "react"
+import {Script} from "gatsby"
 import { Layout } from "./src/components/layout"
 import { ThemeProvider } from '@mui/material/styles';
 import clientTheme from "./src/gatsby-theme-material-ui-top-layout/theme"
@@ -8,6 +9,18 @@ import { CssBaseline } from '@mui/material';
 const wrapPageElement = ({ element, props }) => (
   <ThemeProvider theme={clientTheme}>
     <CssBaseline />
+    <Script
+  src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GTAG}`}
+  strategy="off-main-thread"
+/>
+<Script id="gtag-config" strategy="off-main-thread" forward={[`gtag`]}>
+  {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments)};
+    gtag('js', new Date());
+    gtag('config', ${process.env.GTAG}, { page_path: location ? location.pathname + location.search + location.hash : undefined })
+  `}
+</Script>
     <Layout {...props}>{element}</Layout>
   </ThemeProvider>
 )
