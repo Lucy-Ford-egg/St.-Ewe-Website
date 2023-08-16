19671aec6429fa53ce1f763f71229a40d755710d
Ed Wilson <ed@gendall.co.uk>
2023-08-16 12:55:02 +0100

    disable ripple

--- web/src/utils/navigationLink.js
+++ web/src/utils/navigationLink.js
@@ -15,12 +15,12 @@ export const NavigationLink = ({text, navigationItemUrl, buttonType = "contained
   let linkType = '';
 
   if(navigationItemUrl?.internalnavigationItemUrl !== null){
-    linkType = <Button sx={{...animationHover('white'), textAlign: {xs: "left", md: "center"}}} variant={buttonType} color={color} component={Link} to={navigationItemUrl?.internalnavigationItemUrl.reference?.slug.current}  aria-label={`Link to ${text}`}>
+    linkType = <Button sx={{...animationHover('white'), textAlign: {xs: "left", md: "center"}}} variant={buttonType} color={color} component={Link} to={navigationItemUrl?.internalnavigationItemUrl.reference?.slug.current}  aria-label={`Link to ${text}`} disableRipple={true}>
           {text}
         </Button>
   }
   else{
-    linkType = <Button sx={{textAlign: 'center' }} variant={buttonType} color={color} href={navigationItemUrl.externalnavigationItemUrl?.href} aria-label={`Link to ${text}`}>{text}</Button>
+    linkType = <Button sx={{textAlign: 'center' }} variant={buttonType} color={color} href={navigationItemUrl.externalnavigationItemUrl?.href} aria-label={`Link to ${text}`} disableRipple={true}>{text}</Button>
   }
   return(linkType)
 }
