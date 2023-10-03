f9afa0c15a8d05e8924bbb2a9946eb17cca52bee
Ed Wilson <ed@gendall.co.uk>
2023-10-03 10:46:28 +0100

    removed dead code

--- studio/sanity.config.js
+++ studio/sanity.config.js
@@ -5,48 +5,13 @@ import {schemaTypes} from './schemas'
 import { Logo } from './plugins/logo/logo'
 import { googleMapsInput } from "@sanity/google-maps-input"
 import {media} from 'sanity-plugin-media'
-// import {structure, defaultDocumentNode} from './structure/deskStructure'
-import deskStructure from './structure/deskStructure'
-import {resolveProductionUrl} from './previews/resolveProductionUrl'
-
-// import {createClient} from '@sanity/client'
-
-// const client = createClient({
-//   ...config,
-//   useCdn: false, // must be false for 'previewDrafts'
-//   perspective: 'previewDrafts', // 'raw' | 'published' | 'previewDrafts' 
-// })
+import {deskStructure} from './structure/deskStructure'
 
 export default defineConfig({
   name: 'default',
   title: 'Architectural Holidays',
   projectId: 'e1fodg96',
   dataset: 'production',
-  // document: {
-  //   // prev is the result from previous plugins and thus can be composed
-  //   productionUrl: async (prev, context) => {
-  //     // context includes the client and other details
-  //     const {getClient, dataset, document} = context
-  //     const client = getClient({apiVersion: '2023-05-31'})
-  //     console.log("doc - ", document)
-  //     if (document._type === 'post' || document._type === 'page' || document._type === 'feature' || document._type === 'place') {
-       
-  //       // const slug = await client.fetch(
-  //       //   `*[_type == 'routeInfo' && post._ref == $postId][0].slug.current`,
-  //       //   {postId: document._id}
-  //       // )
-       
-  //       const params = new URLSearchParams()
-  //       params.set('preview', 'true')
-  //       params.set('dataset', dataset)
-  //       const url = resolveProductionUrl(document)
-  //       console.log("url - ", url)
-  //       return `${url}?${params}`
-  //     }
-
-  //     return prev
-  //   },
-  // },
   plugins: [
     deskTool({ structure: deskStructure }),
     visionTool(),
