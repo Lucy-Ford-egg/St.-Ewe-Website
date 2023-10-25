411ee8ddfcddd3332d134ee220c7e6127c800d7d
Ed Wilson <ed@gendall.co.uk>
2023-10-25 08:15:38 +0100

    footer nav breaking dev for the mo

--- web/src/components/footer.js
+++ web/src/components/footer.js
@@ -7,82 +7,83 @@ import { FaInstagram, FaPinterestP } from "react-icons/fa";
 
 export const Footer = () => {
 
-  const data = useStaticQuery(graphql`
-  query FooterMenu {
-    footerMenu: sanityNavigation(navId: {current: {eq: "footer-menu"}}) {
-      items {
-        text
-        navigationItemUrl {
-          externalUrl
-        }
-      }
-    }
-    socialMenu: sanityNavigation(navId: {current: {eq: "social-menu"}}) {
-      items {
-        text
-        navigationItemUrl {
-          externalUrl
-        }
-      }
-    }
-  }
-  `)
-  const renderSocialIcon = (node) => {
+  // const data = useStaticQuery(graphql`
+  // query FooterMenu {
+  //   footerMenu: sanityNavigation(navId: {current: {eq: "footer-menu"}}) {
+  //     items {
+  //       text
+  //       navigationItemUrl {
+  //         externalUrl
+  //       }
+  //     }
+  //   }
+  //   socialMenu: sanityNavigation(navId: {current: {eq: "social-menu"}}) {
+  //     items {
+  //       text
+  //       navigationItemUrl {
+  //         externalUrl
+  //       }
+  //     }
+  //   }
+  // }
+  // `)
+  // const renderSocialIcon = (node) => {
    
-    if(node.text.toLowerCase() === 'instagram' ) {
-      return <IconButton href={node.navigationItemUrl.externalUrl} color="white" aria-label="instagram link"><FaInstagram /></IconButton>
-    }
-    if(node.text.toLowerCase() === 'pinterest'){
-      return <IconButton href={node.navigationItemUrl.externalUrl} color="white" aria-label="pinterest link"><FaPinterestP /></IconButton>
-    }
-    return false
+  //   if(node.text.toLowerCase() === 'instagram' ) {
+  //     return <IconButton href={node.navigationItemUrl.externalUrl} color="white" aria-label="instagram link"><FaInstagram /></IconButton>
+  //   }
+  //   if(node.text.toLowerCase() === 'pinterest'){
+  //     return <IconButton href={node.navigationItemUrl.externalUrl} color="white" aria-label="pinterest link"><FaPinterestP /></IconButton>
+  //   }
+  //   return false
   
-  }
-  return (
-    <Container maxWidth="fluid" disableGutters={true} sx={{ backgroundColor: 'secondary.main', mt: {xs: 10, md: 11}, py: { xs: 9, md: 9 } }}>
+  // }
+  // return (
+  //   <Container maxWidth="fluid" disableGutters={true} sx={{ backgroundColor: 'secondary.main', mt: {xs: 10, md: 11}, py: { xs: 9, md: 9 } }}>
 
-      <Container maxWidth="xl">
-        <Grid container rowSpacing={6}>
+  //     <Container maxWidth="xl">
+  //       <Grid container rowSpacing={6}>
           
-          <Grid item xs={6} md={3} sx={{order: {xs: 2, md: 1}}}>
-            <Box sx={{ ml: { xs: -5, md: 0 }, display: 'flex', alignItems: 'center', justifyContent: {xs: 'flex-end', md: 'flex-start'} }} >
-            {data?.socialMenu?.items && data.socialMenu.items.map((node, i) => {
-              return (
-                <Box key={`footer-menu-item-${i}`} >
-                  {renderSocialIcon(node)}
-                  {/* <NavigationLink text={node.text} linkGroup={node} buttonType="text" color="white" /> */}
-                  </Box>
-              )
-            })}
-            </Box>
-          </Grid>
+  //         <Grid item xs={6} md={3} sx={{order: {xs: 2, md: 1}}}>
+  //           <Box sx={{ ml: { xs: -5, md: 0 }, display: 'flex', alignItems: 'center', justifyContent: {xs: 'flex-end', md: 'flex-start'} }} >
+  //           {data?.socialMenu?.items && data.socialMenu.items.map((node, i) => {
+  //             return (
+  //               <Box key={`footer-menu-item-${i}`} >
+  //                 {renderSocialIcon(node)}
+  //                 {/* <NavigationLink text={node.text} linkGroup={node} buttonType="text" color="white" /> */}
+  //                 </Box>
+  //             )
+  //           })}
+  //           </Box>
+  //         </Grid>
 
-          <Grid item xs={6} md={9} sx={{order: {xs: 1, md: 2}}}>
-            <Box display="flex" sx={{ order: { xs: 0, md: 1 }, flexDirection: { xs: 'column', md: 'row' }, justifyContent: { xs: 'flex-start', md: 'flex-end' } }}>
-              {data?.footerMenu?.items && data.footerMenu.items.map((node, i) => {
-                return (
-                  <Box key={`footer-menu-item-${i}`} sx={{ ml: { xs: -5, md: 0 } }} >
-                    <NavigationLink text={node.text} linkGroup={node} buttonType="text" color="white" />
-                  </Box>
-                )
-              })}
-            </Box>
-          </Grid>
+  //         <Grid item xs={6} md={9} sx={{order: {xs: 1, md: 2}}}>
+  //           <Box display="flex" sx={{ order: { xs: 0, md: 1 }, flexDirection: { xs: 'column', md: 'row' }, justifyContent: { xs: 'flex-start', md: 'flex-end' } }}>
+  //             {data?.footerMenu?.items && data.footerMenu.items.map((node, i) => {
+  //               return (
+  //                 <Box key={`footer-menu-item-${i}`} sx={{ ml: { xs: -5, md: 0 } }} >
+  //                   <NavigationLink text={node.text} linkGroup={node} buttonType="text" color="white" />
+  //                 </Box>
+  //               )
+  //             })}
+  //           </Box>
+  //         </Grid>
 
-          <Grid item xs={12} md={5} sx={{order: {xs: 4, md: 3}}}>
-            <Box sx={{ display: 'flex' }}>
-              <Typography color="primary.main" variant="caption">{`Copyright © ${new Date().getFullYear()}  Heligan Campsite  |  Designed & Developed by `} <a className="link-animation" href="https://www.gendall.co.uk">Gendall</a></Typography>
-            </Box>
-          </Grid>
-          <Grid item xs={12} md={7} sx={{order: {xs: 3, md: 4}}}>
-            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: {xs: 'flex-start', md: 'flex-end'} }} >
-              <Typography sx={{fontFamily: 'Montserrat', color: 'rgba(255,255,255, 0.6)'}} variant="h6">Accreditation &amp; Affiliations</Typography>
-            </Box>
-          </Grid>
+  //         <Grid item xs={12} md={5} sx={{order: {xs: 4, md: 3}}}>
+  //           <Box sx={{ display: 'flex' }}>
+  //             <Typography color="primary.main" variant="caption">{`Copyright © ${new Date().getFullYear()}  Heligan Campsite  |  Designed & Developed by `} <a className="link-animation" href="https://www.gendall.co.uk">Gendall</a></Typography>
+  //           </Box>
+  //         </Grid>
+  //         <Grid item xs={12} md={7} sx={{order: {xs: 3, md: 4}}}>
+  //           <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: {xs: 'flex-start', md: 'flex-end'} }} >
+  //             <Typography sx={{fontFamily: 'Montserrat', color: 'rgba(255,255,255, 0.6)'}} variant="h6">Accreditation &amp; Affiliations</Typography>
+  //           </Box>
+  //         </Grid>
 
-        </Grid>
-      </Container>
+  //       </Grid>
+  //     </Container>
 
-    </Container>
-  )
+  //   </Container>
+  // )
+  return <></>
 }
