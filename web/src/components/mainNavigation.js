691984fc1ebd82331729d0adf3e63a2e1a24b51e
Ed Wilson <ed@gendall.co.uk>
2023-10-25 08:17:00 +0100

    formtating

--- web/src/components/mainNavigation.js
+++ web/src/components/mainNavigation.js
@@ -1,26 +1,26 @@
 import React from "react"
 
-import { Box} from "@mui/material"
+import { Box } from "@mui/material"
 
 import { NavigationItem } from "./navigationItem"
 
 
 const MainNavigation = (props) => {
- 
-  const {handleCloseNavMenu, menu} = props
+
+  const { handleCloseNavMenu, menu } = props
 
   return (
-    <Box sx={{ flexGrow: 1, display: { lg: 'flex' }, flexDirection: {xs: 'column', md: 'row'}, justifyContent: {xs: 'flex-start', md: 'flex-end'} }}>
+    <Box sx={{ flexGrow: 1, display: { lg: 'flex' }, flexDirection: { xs: 'column', md: 'row' }, justifyContent: { xs: 'flex-start', md: 'flex-end' } }}>
 
       {menu?.sanityNavigation?.items && menu.sanityNavigation.items.map((menuItem, i) => {
-      
+
         return (
-          <NavigationItem  
-            handleCloseNavMenu={handleCloseNavMenu} 
-            key={`main-menu-item-${i}`} 
-            link={menuItem?.link?.link} 
-            childNode={menuItem?.childItems} 
-            text={menuItem?.link?.text}/>
+          <NavigationItem
+            handleCloseNavMenu={handleCloseNavMenu}
+            key={`main-menu-item-${i}`}
+            link={menuItem?.link?.link}
+            childNode={menuItem?.childItems}
+            text={menuItem?.link?.text} />
         )
       })
       }
