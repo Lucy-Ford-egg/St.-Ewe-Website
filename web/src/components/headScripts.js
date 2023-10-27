c27b27f7df56c69e66a97346d6b4bfe056293a21
Ed Wilson <ed@gendall.co.uk>
2023-10-27 08:18:19 +0100

    check if previewing

--- web/src/components/headScripts.js
+++ web/src/components/headScripts.js
@@ -4,17 +4,18 @@ import { Script } from "gatsby"
 export const HeadScripts = (props) => {
   const {activePreview} = props
 
-  const [preview, setPreview] = useState(false)
+  const [preview, setPreview] = useState(activePreview || null)
 
   useEffect(() => {
     setPreview( activePreview)
 
   }, [activePreview])
-  
+  debugger
 
+console.log(`Preview Active = ${preview}`)
   return (
     <>
-      {/* {!preview ? 
+      {!preview ? 
         <>
           <Script
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GATSBY_GTAG}`}
@@ -23,7 +24,7 @@ export const HeadScripts = (props) => {
           />
           <Script
             id="gtag-config"
-            strategy="off-main-thread"
+            strat egy="off-main-thread"
             dangerouslySetInnerHTML={{
               __html: `window.dataLayer = window.dataLayer || [];
           window.gtag = function gtag(){ window.dataLayer.push(arguments);}
@@ -31,8 +32,11 @@ export const HeadScripts = (props) => {
           gtag('config', '${process.env.GATSBY_GTAG}', { send_page_view: false, page_path: location ? location.pathname + location.search + location.hash : undefined })`,
             }}
           />
+          <Script id="gemaParkSearch" src={`https://bookings.gemapark.co.uk/javascript/gemapark_5_0_s.js?cid=${process.env.GATSBY_GEMAPARK_CID}`}></Script>
+         
         </>
-      : <></>} */}
+        
+      : <></>}
     </>
   )
 }
