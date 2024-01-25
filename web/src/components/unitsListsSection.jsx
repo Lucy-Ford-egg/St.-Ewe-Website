32b9783279bf8889870359d0cb18d5d46efc9497
Ed Wilson <ed@gendall.co.uk>
2024-01-25 14:23:04 +0000

    iframe resize out. Might not be able to use.

--- web/src/components/unitsListsSection.jsx
+++ web/src/components/unitsListsSection.jsx
@@ -3,7 +3,6 @@ import { graphql } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import { getGatsbyImageData } from "gatsby-source-sanity"
 import { Container, Typography, Box, useTheme } from "@mui/material"
-import IframeResizer from 'iframe-resizer-react'
 
 export const UnitsListsSection = props => {
   const theme = useTheme()
@@ -11,13 +10,7 @@ export const UnitsListsSection = props => {
 
   const iframeRef = useRef(null)
   const [messageData, setMessageData] = useState()
-
-  const onResized = data => setMessageData(data)
-
-  const onMessage = data => {
-    setMessageData(data)
-    iframeRef.current.sendMessage("Hello back from the parent page")
-  }
+  const [height, setHeight] = useState("0px")
 
   return (
     <Container
@@ -25,24 +18,46 @@ export const UnitsListsSection = props => {
       sx={{
         display: "flex",
         justifyItems: "center",
+        height: "100%",
       }}
     >
       <Box
         sx={{
           px: { xs: 0, sm: theme.spacing(12) },
           width: "100%",
-          // display: "flex",
-          // justifyItems: "center",
-          // position: "relative",
-          // height: "fit-content",
+          //       position: 'relative',
+          // paddingBottom: '56.25%', /* 16:9 */
+          // paddingTop: '25px',
+          // height: 0,
         }}
       >
-        <IframeResizer
+        <iframe
+          id="ifrResults"
+          class="gemaResults"
+          height="1300px"
+          width="100%"
+          title="property-results"
+          src={`https://bookings.gemapark.co.uk/park/searchresults.aspx?cid=${process.env.GATSBY_GEMAPARK_CID}&amp;curid=1&amp;cul=en-GB&amp;ifr=ifrResults&amp;url=https%3a%2f%2f${process.env.GATSBY_GEMAPARK_URL}%2fsearch-results.html`}
+          style={{
+            width: "1px",
+            minWidth: "100%",
+            // position: "absolute",
+            // top: 0,
+            // left: 0,
+            // width: "100%",
+            // height: "100%",
+          }}
+        />
+        {/* <IframeResizer
+          log
           forwardRef={iframeRef}
-          heightCalculationMethod="taggedElement"
+          autoResize={true}
+          bodyBackground={true}
+          bodyMargin={true}
+          bodyPadding={true}
+          heightCalculationMethod="bodyOffset"
           sizeHeight={true}
           inPageLinks
-          log
           warningTimeout={0}
           onMessage={onMessage}
           onResized={onResized}
@@ -51,28 +66,8 @@ export const UnitsListsSection = props => {
           src={`https://bookings.gemapark.co.uk/park/searchresults.aspx?cid=${process.env.GATSBY_GEMAPARK_CID}&amp;curid=1&amp;cul
   =en-GB&amp;ifr=ifrResults&amp;url=https%3a%2f%2f${process.env.GATSBY_GEMAPARK_URL}%2fsearch-
   results.html`}
-          style={{ width: "1px", minWidth: "100%" }}
-        />
-        {/* <iframe
-          id="ifrResults"
-          
-          
-          style={{ 
-            // position: "absolute",
-            // height: "100vh", 
-            // width: "100%", 
-            // border: "none", 
-            // left: 0, 
-            // top: 0 
-          }}
-          border="0"
-          marginwidth="0"
-          marginheight="0"
-          frameborder="0"
-          source={`https://bookings.gemapark.co.uk/park/searchresults.aspx?cid=${process.env.GATSBY_GEMAPARK_CID}&amp;curid=1&amp;cul
-=en-GB&amp;ifr=ifrResults&amp;url=https%3a%2f%2f${process.env.GATSBY_GEMAPARK_URL}%2fsearch-
-results.html`}
-        ></iframe> */}
+  style={{ width: "1px", minWidth: "100%" }}
+        /> */}
       </Box>
     </Container>
   )
