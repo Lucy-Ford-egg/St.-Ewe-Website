6583a7c7e0eefdc336de945cabcc43d17f02987a
Ed Wilson <ed@gendall.co.uk>
2024-01-25 14:21:47 +0000

    seo schema back in

--- studio/schemas/page.js
+++ studio/schemas/page.js
@@ -1,6 +1,6 @@
-import { MdAutoStories } from "react-icons/md";
-import { format, parseISO } from 'date-fns'
-import { defineField, defineType } from 'sanity'
+import {MdAutoStories} from 'react-icons/md'
+import {format, parseISO} from 'date-fns'
+import {defineField, defineType} from 'sanity'
 import openGraph from '../schemas/openGraph'
 import siteMeta from '../schemas/siteMeta'
 
@@ -11,14 +11,14 @@ import featureSectionType from './modules/featureSection'
 import videoSectionType from './modules/videoSection'
 import featuresListSectionType from './modules/featuresListSection'
 import ctaSectionType from './modules/ctaSection'
-import servicesSectionType from "./modules/servicesSection"
-import testimonialSectionType from "./modules/testimonialSection"
-import imageCarouselSectionType from "./modules/imageCarouselSection"
-import locationSectionType from "./modules/locationSection"
-import faqsSectionType from "./modules/faqsSection"
-import benifitsSectionType from "./modules/benifitsSection"
+import servicesSectionType from './modules/servicesSection'
+import testimonialSectionType from './modules/testimonialSection'
+import imageCarouselSectionType from './modules/imageCarouselSection'
+import locationSectionType from './modules/locationSection'
+import faqsSectionType from './modules/faqsSection'
+import benifitsSectionType from './modules/benifitsSection'
 import contactSectionType from './modules/contactSection'
-import blogSectionType from "./modules/blogSection";
+import blogSectionType from './modules/blogSection'
 
 // import imageWithCaptionType from './modules/imageWithCaption'
 // import textBlockType from './modules/textBlock'
@@ -35,8 +35,6 @@ import blogSectionType from "./modules/blogSection";
 // import imageWithLinkType from '../schemas/modules/imageWithLink'
 // import testimonialSection from "./modules/testimonialSection";
 
-
-
 /**
  * This file is the schema definition for a post.
  *
@@ -48,8 +46,8 @@ import blogSectionType from "./modules/blogSection";
  */
 
 export default defineType({
-  name: "page",
-  title: "Page",
+  name: 'page',
+  title: 'Page',
   icon: MdAutoStories,
   type: 'document',
   groups: [
@@ -58,23 +56,28 @@ export default defineType({
       title: 'Page Content',
       default: true,
     },
-    // {
-    //   name: 'og',
-    //   title: 'SEO',
-    // },
-    // {
-    //   name: 'meta',
-    //   title: 'Page Meta'
-    // }
+    {
+      name: 'og',
+      title: 'SEO',
+    },
+    {
+      name: 'meta',
+      title: 'Page Meta',
+    },
+    {
+      name: 'seo',
+      title: 'Seo Details',
+    },
   ],
   fields: [
-    // ...siteMeta.fields, ...openGraph.fields,
-    // {name: 'anothertitle', title: 'Title', type: 'string'},
-    // {name: 'icon', title: 'Icon', type: 'image'},
-    // {name: 'seoTitle', title: 'SEO title', type: 'string', group: 'seo'},
-    // {name: 'seoKeywords', title: 'Keywords', type: 'string', group: 'seo'},
-    // {name: 'seoSlug', title: 'Slug', type: 'slug', group: 'seo'},
-    // {name: 'seoImage', title: 'Image', type: 'image', group: 'seo'},
+    ...siteMeta.fields,
+    ...openGraph.fields,
+    // defineField({name: 'anothertitle', title: 'Title', type: 'string'}),
+    // defineField({name: 'icon', title: 'Icon', type: 'image'}),
+    // defineField({name: 'seoTitle', title: 'SEO title', type: 'string', group: 'seo'}),
+    // defineField({name: 'seoKeywords', title: 'Keywords', type: 'string', group: 'seo'}),
+    // defineField({name: 'seoSlug', title: 'Slug', type: 'slug', group: 'seo'}),
+    // defineField({name: 'seoImage', title: 'Image', type: 'image', group: 'seo'}),
     defineField({
       name: 'pageTitle',
       title: 'Page Title',
@@ -87,7 +90,8 @@ export default defineType({
       name: 'slug',
       title: 'Slug',
       type: 'slug',
-      description: 'The slug is what your page name is called in url. Either click the generate button to format your page title into hyphenated lowercase or type your own slug. (when typing your own slug use caution not to use a duplicate slug)',
+      description:
+        'The slug is what your page name is called in url. Either click the generate button to format your page title into hyphenated lowercase or type your own slug. (when typing your own slug use caution not to use a duplicate slug)',
       options: {
         source: 'pageTitle',
         maxLength: 96,
@@ -101,7 +105,7 @@ export default defineType({
       title: 'Date',
       type: 'datetime',
       initialValue: () => new Date().toISOString(),
-      group: 'pageContent'
+      group: 'pageContent',
     }),
     // defineField({
     //   name: 'categoryArchive',
@@ -114,22 +118,23 @@ export default defineType({
       name: 'pageBuilder',
       type: 'array',
       title: 'Page builder',
-      description: 'Build out the structure of the page sections by clicking add item and selecting the module which best suits the type of content you wish to add.',
+      description:
+        'Build out the structure of the page sections by clicking add item and selecting the module which best suits the type of content you wish to add.',
       of: [
-        { type: headerSectionAccommodationSearchType.name },
-        { type: unitsListsSectionType.name },
-        { type: featureSectionType.name },
-        { type: videoSectionType.name },
-        { type: featuresListSectionType.name },
-        { type: ctaSectionType.name },
-        { type: servicesSectionType.name },
-        { type: testimonialSectionType.name },
-        { type: imageCarouselSectionType.name },
-        { type: locationSectionType.name },
-        { type: faqsSectionType.name },
-        { type: benifitsSectionType.name },
-        { type: contactSectionType.name },
-        { type: blogSectionType.name },
+        {type: headerSectionAccommodationSearchType.name},
+        {type: unitsListsSectionType.name},
+        {type: featureSectionType.name},
+        {type: videoSectionType.name},
+        {type: featuresListSectionType.name},
+        {type: ctaSectionType.name},
+        {type: servicesSectionType.name},
+        {type: testimonialSectionType.name},
+        {type: imageCarouselSectionType.name},
+        {type: locationSectionType.name},
+        {type: faqsSectionType.name},
+        {type: benifitsSectionType.name},
+        {type: contactSectionType.name},
+        {type: blogSectionType.name},
         // etc...
       ],
       group: 'pageContent',
@@ -142,13 +147,13 @@ export default defineType({
       date: 'date',
       media: 'coverImage',
     },
-    prepare({ title, media, location, date }) {
+    prepare({title, media, location, date}) {
       const subtitles = [
         location && `in ${location}`,
         date && `published on ${format(parseISO(date), 'LLL d, yyyy')}`,
       ].filter(Boolean)
 
-      return { title, media, subtitle: subtitles.join(' ') }
+      return {title, media, subtitle: subtitles.join(' ')}
     },
   },
 })
