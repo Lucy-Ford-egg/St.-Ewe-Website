76542d3b5c8ca0003c169692af99b0959536df43
Ed Wilson <ed@gendall.co.uk>
2024-01-25 14:22:04 +0000

    seo pane preview

--- studio/structure/views/preview.jsx
+++ studio/structure/views/preview.jsx
@@ -1,13 +1,19 @@
 import IframePreview from '../previews/iframePreview'
-// import {SEOPane} from 'sanity-plugin-seo-pane'
-// import {getUrl} from '../previews/iframePreview'
+import {SEOPane} from 'sanity-plugin-seo-pane'
 
 const remoteURL = 'https://heligancampsite.netlify.app' // ! For some reason env.vars aren't working process.env.SANITY_STUDIO_FRONTEND // either production or a specialized deploy branch for previews
-const localURL = 'http://localhost:8000' // local development port from Gatsby
+const localURL = 'https://localhost:8000' // local development port from Gatsby
 const previewUrl = window.location.hostname.includes('localhost') ? localURL : remoteURL
 
+
+const resolveProductionUrl = (doc) => {
+  console.log(`RPU - ${previewUrl}`)
+  return previewUrl
+}
 // const previewUrl = process.env.SANITY_STUDIO_FRONTEND
 export const Views = (S, context) => {
+
+  console.log(`previewUrl = ${previewUrl}`)
   return [
     S.view.form(), // Default Sanity's form view for editing content
     S.view
@@ -18,32 +24,14 @@ export const Views = (S, context) => {
       .component(IframePreview)
       .options({previewUrl, isMobilePreview: true, context})
       .title('Mobile Preview'), // Iframe custom view for mobile screens
-      
-    // S.view
-    //   .component(SEOPane)
-    //   .options({
-    //     // Retrieve the keywords and synonyms at the given dot-notated strings
-    //     keywords: `seo.keywords`,
-    //     synonyms: `seo.synonyms`,
-    //     url: async(document) => {
-    //       const response = await fetch(previewUrl)
-    //       const url = response;
-    //       console.log(`What url - ${JSON.stringify(url)}`)
-    //       return url
-    //     }
-
-    //     // url: async (document) => {
-    //     //   // console.log(`Document Preview ${JSON.stringify(document)}`)
-    //     //   const displayed = document
-          
-    //     //   return getUrl({ previewUrl, displayed, context })
-    //     // },
-
-    //     // Alternatively, specify functions (may be async) to extract values
-    //     // keywords: doc => doc.seo?.keywords,
-    //     // synonyms: async(doc) => client.fetch('some query to get synonyms', {id: doc._id}),
-    //     // url: async(doc) => client.fetch('some query to construct a url with refs', {id: doc._id})
-    //   })
-    //   .title('SEO'),
+    S.view
+      .component(SEOPane)
+      .options({
+        // Retrieve the keywords and synonyms at the given dot-notated strings
+        keywords: `seo.keywords`,
+        synonyms: `seo.synonyms`,
+        url: (doc) => resolveProductionUrl(doc),
+      })
+      .title('SEO'),
   ]
 }
