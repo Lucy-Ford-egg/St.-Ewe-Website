416c01545134fa2fd2d97a0bcc2fdc5285b7214f
Ed Wilson <ed@gendall.co.uk>
2024-01-25 14:21:22 +0000

    new admin logo

--- studio/plugins/logo/logo.jsx
+++ studio/plugins/logo/logo.jsx
@@ -1,3 +1,9 @@
-export const Logo = () => (
-  <img src="../static/cms-logo.svg" alt="Heligan Campsite"/>
-)
+import cmsLogo from '../../static/cms-logo.svg'
+
+export const Logo = () => {
+  return (
+    <div style={{width: 25}}>
+     <img style={{maxWidth: '100%', height: 'auto'}} src={cmsLogo} alt="Heligan Campsite"/>
+    </div>
+  )
+}
