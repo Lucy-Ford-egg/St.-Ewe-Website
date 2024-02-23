86ebb0139ce4308a7a2bce5c9f77ce526d8caaf7
Ed Wilson <ed@gendall.co.uk>
2024-02-23 12:03:33 +0000

    not used

--- studio/previews/resolveProductionUrl.js
+++
@@ -1,4 +0,0 @@
-export const resolveProductionUrl = (document) => {
-  const url = process.env.SANITY_STUDIO_FRONTEND// window.location.hostname.includes("localhost") ? "http://localhost:8000" : "https://architecturalholidays.netlify.app"
-  return url
-}
