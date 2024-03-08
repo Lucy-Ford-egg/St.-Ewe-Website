0e9de1eba03e55302bdd56f469c8a2c34931547c
Ed Wilson <ed@gendall.co.uk>
2024-03-08 11:43:36 +0000

    validation to portable text image

--- studio/schemas/post.js
+++ studio/schemas/post.js
@@ -190,7 +190,8 @@ export default defineType({
         ],
       }
     }, {
-      type: 'image'
+      type: 'image',
+      validation: (rule) => rule.required(),
     }],
     validation: (rule) => rule.required(),
     description: "This works slightly differently to page modules. You can add images, quotes and other textural decoration in the editor.",
