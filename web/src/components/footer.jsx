9794873dfb6b704baf31a09a8ae5330dc6060067
Ed Wilson <ed@gendall.co.uk>
2024-03-08 22:43:06 +0000

    wording errors

--- web/src/components/footer.jsx
+++ web/src/components/footer.jsx
@@ -698,10 +698,9 @@ export const Footer = props => {
               sx={{ mb: { xs: 6, sm: 0 }, display: "flex" }}
             >
               We use cookies to make the site work better, but also to see how
-              you interact with it. how you interact with it. We will only use
-              cookies if you allow us to do so by clicking by clicking on
-              "Accept Cookies". You can also choose which cookie you want to
-              allow.
+              you interact with it. We will only use
+              cookies if you allow us to do so by clicking on
+              "Accept Cookies".
             </Typography>
           </Box>
         </CookieNotice>
