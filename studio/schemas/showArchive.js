fdb8568ce1f0dfdce9345a7c0d212a9ec20fbfd5
Ed Wilson <ed@gendall.co.uk>
2024-04-23 12:35:34 +0100

    remove description incorrect

--- studio/schemas/showArchive.js
+++ studio/schemas/showArchive.js
@@ -4,7 +4,7 @@ export default defineType({
       name: 'showArchive',
       type: 'object',
       title: 'Show an archive of blog posts on page.',
-      description: 'This inserts a blog grid in the 2nd postion in the module builder',
+      //description: 'This inserts a blog grid in the 2nd postion in the module builder',
       //description: `Select this to page the a page with all the blog posts. Example: This is used to make the blog page but could also be used to make another page containing blog posts. This will then use a template which inserts the the blog posts for this template directly underneath the first component.`,
       group: 'pageContent',
       options: {
