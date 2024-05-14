8f087a93e649df30f6e7cb1911397a650465058f
Ed Wilson <ed@gendall.co.uk>
2024-05-14 15:33:15 +0100

    ooo react lazy maybe?

--- web/src/components/modules.jsx
+++ web/src/components/modules.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-import { BlogSection } from "./blogSection"
-import { HeaderSection } from './headerSection'
-import { TimelineSection } from './timelineSection'
-import { VideoSection } from './videoSection'
-import { FeaturesListSection } from './featuresListSection'
-import { CtaSection } from './ctaSection'
-import { TestimonialSection } from './testimonialSection'
-import { ImageCarouselSection } from './imageCarouselSection'
-import { LocationSection } from './locationSection'
-import { ContactSection } from './contactSection'
-import { TeamSection } from './teamSection'
-import { CaseStudySection} from './caseStudySection'
-import { NewsletterSection } from './newsletterSection'
-import { StepsSection } from './stepsSection'
-import { ClientLoginSection } from './clientLoginSection'
+const BlogSection= React.lazy(() =>  import("./blogSection").then(module => ({ default: module.BlogSection })));
+const HeaderSection = React.lazy(() =>  import('./headerSection').then(module => ({ default: module.HeaderSection })));
+const TimelineSection = React.lazy(() =>  import('./timelineSection').then(module => ({ default: module.TimelineSection })));
+const VideoSection= React.lazy(() =>  import('./videoSection').then(module => ({ default: module.VideoSection })));
+const FeaturesListSection= React.lazy(() =>  import('./featuresListSection').then(module => ({ default: module.FeaturesListSection })));
+const CtaSection= React.lazy(() =>  import('./ctaSection').then(module => ({ default: module.CtaSection })));
+const TestimonialSection= React.lazy(() =>  import('./testimonialSection').then(module => ({ default: module.TestimonialSection })));
+const ImageCarouselSection= React.lazy(() =>  import('./imageCarouselSection').then(module => ({ default: module.ImageCarouselSection })));
+const LocationSection= React.lazy(() =>  import('./locationSection').then(module => ({ default: module.LocationSection })));
+const ContactSection= React.lazy(() =>  import('./contactSection').then(module => ({ default: module.ContactSection })));
+const TeamSection= React.lazy(() =>  import('./teamSection').then(module => ({ default: module.TeamSection })));
+const CaseStudySection = React.lazy(() =>  import('./caseStudySection').then(module => ({ default: module.CaseStudySection })));
+const NewsletterSection= React.lazy(() =>  import('./newsletterSection').then(module => ({ default: module.NewsletterSection })));
+const StepsSection= React.lazy(() =>  import('./stepsSection').then(module => ({ default: module.StepsSection })));
+const ClientLoginSection= React.lazy(() =>  import('./clientLoginSection').then(module => ({ default: module.ClientLoginSection })));
 
 const Modules = (props) => {
 
@@ -32,7 +32,7 @@ const Modules = (props) => {
     if (modules != null) {
       
         return (
-            
+            <React.Suspense fallback="Loading...">
             <main data-content="main">
                 
                 {modules && modules.map((module, i) => {
@@ -192,7 +192,7 @@ const Modules = (props) => {
 
                 })}
             </main>
-
+            </React.Suspense>
         )
     } else {
         return null
