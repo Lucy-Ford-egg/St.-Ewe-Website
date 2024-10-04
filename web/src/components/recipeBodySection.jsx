b00a2c6efd3e682a6df2584f4f50287529904d14
Ed Wilson <hello@edwardwilson.co>
2024-10-04 11:53:07 +0100

    texture on recipie instructions

--- web/src/components/recipeBodySection.jsx
+++ web/src/components/recipeBodySection.jsx
@@ -28,6 +28,7 @@ const ListWrapper = styled('div')(({ theme, borderDirection, backgroundColour, j
   gridColumn: '14/23',
   display: 'grid',
   gridTemplateColumns: 'repeat(24, 1fr)',
+  gridTemplateRows: '51px auto 51px',
   [theme.breakpoints.up('sm')]: {
 
   }
@@ -36,14 +37,21 @@ const ListWrapper = styled('div')(({ theme, borderDirection, backgroundColour, j
 
 const List = styled('div')(({ theme, borderDirection, backgroundColour, joiningColour, mirror }) => ({
   gridColumn: '1/25',
-  display: 'grid',
+  gridRow: '2/auto',
   "& .title": {
     textTransform: 'uppercase'
   },
   backgroundColor: contrastBrandPalette[backgroundColour?.label]?.contrastBase,
-  padding: 'var(--ms3) var(--ms3)',
+  padding: '0 var(--ms3)',
+  "& ul":{
+    listStyle: 'none',
+    margin: 0,
+    padding: 0,
+  },
   "& li": {
-
+    paddingTop: 'var(--ms-3)',
+    paddingBottom: 'var(--ms-3)',
+    borderBottom: '1px solid var(--original-large)',
   },
   [theme.breakpoints.up('sm')]: {
 
@@ -52,6 +60,9 @@ const List = styled('div')(({ theme, borderDirection, backgroundColour, joiningC
 
 const Border = styled('div')(({ theme, borderDirection, backgroundColour, joiningColour, mirror }) => ({
   gridColumn: '1/25',
+  alignItems: 'end',
+  display: 'flex',
+  height: 'fit-content',
   svg: {
     width: "100%",
     height: 'auto',
@@ -61,9 +72,27 @@ const Border = styled('div')(({ theme, borderDirection, backgroundColour, joinin
         fill: contrastBrandPalette[backgroundColour?.label]?.contrastBase,
     },
   },
-  [theme.breakpoints.up('sm')]: {
-
-  }
+  position: "relative",
+    "&:before": {
+        position: "absolute",
+        content: "''",
+        width: "100%",
+        height: "2px",
+        backgroundColor: contrastBrandPalette[backgroundColour?.label]?.contrastBase,
+        inset: 0,
+        bottom: "-1px",
+        top: "unset",
+    },
+    "&:after": {
+        position: "absolute",
+        content: "''",
+        width: "100%",
+        height: "2px",
+        backgroundColor: contrastBrandPalette[backgroundColour?.label]?.contrastBase,
+        inset: 0,
+        top: "unset",
+        bottom: "-1px",
+    },
 }));
 
 const Content = styled('div')(({ mirror, theme }) => ({
@@ -111,7 +140,7 @@ export const RecipeBodySection = props => {
 
   // const definedImage =
   //   (previewData && _type === previewData?._type && previewData?.image) || image
-  debugger
+
   const definedImage = (image?.asset && image) || (pageContext?.node?.featuredMedia)
 
   const definedBackgroundColour =
@@ -131,22 +160,23 @@ export const RecipeBodySection = props => {
         </Content>
 
         <ListWrapper mirror={mirror} backgroundColour={backgroundColour}>
-          <Border backgroundColour={backgroundColour}><svg width="443" height="36" viewBox="0 0 443 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <Border style={{ gridRow: '1/1'}}
+          backgroundColour={backgroundColour}><svg width="443" height="36" viewBox="0 0 443 36" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M15.7492 10.4279C10.4913 10.5973 5.22845 11.2579 0 12.4266V35.8657L443 35.8657V11.3087C441.463 10.682 439.892 10.0214 438.282 9.36084C436.672 8.70025 435.027 8.03966 433.343 7.46376C429.975 6.27809 426.47 9.05855 422.837 7.78819C408.281 3.09632 391.589 10.5738 373.292 7.4064C354.995 4.17122 335.093 1.88457 314.139 0.800529C293.186 -0.300451 271.178 -0.232698 248.654 0.800529C245.841 0.936034 243.018 1.07154 240.185 1.20704C237.357 1.39336 234.52 1.56275 231.677 1.74907C228.83 1.93538 225.977 2.13864 223.12 2.32496C220.238 2.57903 217.357 2.81617 214.465 3.07024C202.913 4.23897 191.327 0.915846 179.775 2.88067C156.667 6.81032 133.701 4.9087 111.319 9.21099C100.126 11.3791 89.0801 9.36083 78.2403 10.9191C76.8853 11.1224 75.5352 11.3087 74.1901 11.512L70.2135 12.0201C67.5772 12.3419 64.8721 12.7993 62.4861 12.7654L60.6403 12.8332C60.0021 12.8162 59.3589 12.7993 58.7207 12.7993C57.4394 12.7654 56.1728 12.7823 54.8767 12.6976C53.5855 12.6299 52.2993 12.5621 51.0131 12.4944C49.717 12.3928 48.4209 12.3081 47.1297 12.2064C41.8522 11.7491 36.6336 11.3087 31.4886 10.8683C26.2503 10.4788 20.9924 10.2924 15.7492 10.4279Z" fill="#99CBEB"/>
             </svg></Border>
           <List mirror={mirror} backgroundColour={backgroundColour}>
 
             <Typography variant="h2" className="title">Ingredients</Typography>
+            <ul>
             {data?.sanityRecipes?.ingredientsList?.map((item) => {
               return (
                 <li key={item?._key}>{`${item?.ingredient?.wholeNumber ? item?.ingredient?.wholeNumber : ''} ${item?.ingredient?.title} ${item?.ingredient?.preparation ? " - " + item?.ingredient?.preparation : ''}`}</li>
               )
             })}
-
+</ul>
           </List>
-          <Border  backgroundColour={backgroundColour} style={{
-             transform: ' rotate(180deg)',
-          }}><svg width="443" height="36" viewBox="0 0 443 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <Border style={{gridRow: '3/3',  transform: ' rotate(180deg)', }}
+           backgroundColour={backgroundColour}><svg width="443" height="36" viewBox="0 0 443 36" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M15.7492 10.4279C10.4913 10.5973 5.22845 11.2579 0 12.4266V35.8657L443 35.8657V11.3087C441.463 10.682 439.892 10.0214 438.282 9.36084C436.672 8.70025 435.027 8.03966 433.343 7.46376C429.975 6.27809 426.47 9.05855 422.837 7.78819C408.281 3.09632 391.589 10.5738 373.292 7.4064C354.995 4.17122 335.093 1.88457 314.139 0.800529C293.186 -0.300451 271.178 -0.232698 248.654 0.800529C245.841 0.936034 243.018 1.07154 240.185 1.20704C237.357 1.39336 234.52 1.56275 231.677 1.74907C228.83 1.93538 225.977 2.13864 223.12 2.32496C220.238 2.57903 217.357 2.81617 214.465 3.07024C202.913 4.23897 191.327 0.915846 179.775 2.88067C156.667 6.81032 133.701 4.9087 111.319 9.21099C100.126 11.3791 89.0801 9.36083 78.2403 10.9191C76.8853 11.1224 75.5352 11.3087 74.1901 11.512L70.2135 12.0201C67.5772 12.3419 64.8721 12.7993 62.4861 12.7654L60.6403 12.8332C60.0021 12.8162 59.3589 12.7993 58.7207 12.7993C57.4394 12.7654 56.1728 12.7823 54.8767 12.6976C53.5855 12.6299 52.2993 12.5621 51.0131 12.4944C49.717 12.3928 48.4209 12.3081 47.1297 12.2064C41.8522 11.7491 36.6336 11.3087 31.4886 10.8683C26.2503 10.4788 20.9924 10.2924 15.7492 10.4279Z" fill="#99CBEB"/>
           </svg></Border>
         </ListWrapper>
