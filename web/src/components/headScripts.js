bf97d5f658db01d314c240a8a1f91e52d407a565
eddiewilson <eddie_wilson@hotmail.co.uk>
2024-11-04 14:24:53 +0000

    not off main thread

--- web/src/components/headScripts.js
+++ web/src/components/headScripts.js
@@ -24,7 +24,7 @@ export const HeadScripts = props => {
           async
           defer
           src={`https://js-eu1.hs-scripts.com/${process.env.GATSBY_HUBSPOT_ID}.js`}
-          strategy="off-main-thread"
+          //strategy="off-main-thread"
           // forward={[`gtag`]}
         />
         <Script
--- web/wrapPageElement.js
+++ web/wrapPageElement.js
@@ -2,29 +2,23 @@ import React from "react"
 import SanityPreviewConnectorProvider from "./src/components/preview/sanityPreviewConnector"
 import { PreviewProvider } from "./src/context/previewContext"
 import { Layout } from "./src/components/layout"
-import { ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider } from "@mui/material/styles"
 import clientTheme from "./src/gatsby-theme-material-ui-top-layout/theme"
-import { CssBaseline } from '@mui/material';
-import { HeadScripts } from './src/components/headScripts'
-
+import { CssBaseline } from "@mui/material"
+import { HeadScripts } from "./src/components/headScripts"
 
 // Pass all props (hence the ...props) to the layout component so it has access to things like pageContext or location
-const wrapPageElement = ({ element, props}) => {
-  
+const wrapPageElement = ({ element, props }) => {
   return (
-
     <ThemeProvider theme={clientTheme}>
       <CssBaseline />
       <PreviewProvider>
         <SanityPreviewConnectorProvider token={process.env.SANITY_TOKEN}>
           <HeadScripts activePreview={props} />
-          <Layout {...props}>
-            {element}
-          </Layout>
+          <Layout {...props}>{element}</Layout>
         </SanityPreviewConnectorProvider>
       </PreviewProvider>
     </ThemeProvider>
-
   )
 }
 
