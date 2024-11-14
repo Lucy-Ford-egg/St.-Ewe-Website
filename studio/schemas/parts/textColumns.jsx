d20b037095bc15538a06d77f3b6f59da75b81113
eddiewilson <eddie_wilson@hotmail.co.uk>
2024-11-14 14:11:55 +0000

    column description

--- studio/schemas/parts/textColumns.jsx
+++ studio/schemas/parts/textColumns.jsx
@@ -1,6 +1,5 @@
-import { defineField, defineType } from 'sanity'
-import { LiaColumnsSolid } from "react-icons/lia";
-
+import {defineField, defineType} from 'sanity'
+import {LiaColumnsSolid} from 'react-icons/lia'
 
 export default defineType({
   name: 'textColumns',
@@ -14,13 +13,15 @@ export default defineType({
       title: 'Number of Columns',
       options: {
         list: [
-          {title: "1 Column", value: "1"},
-          {title: "2 Columns", value: "2"},
-          {title: "3 Columns", value: "3"},
-          {title: "4 Columns", value: "4"},
+          {title: '1 Column', value: '1'},
+          {title: '2 Columns', value: '2'},
+          {title: '3 Columns', value: '3'},
+          {title: '4 Columns', value: '4'},
           //{title: "Screen", value: "screen", description: "Removes original colour and applies a colour to the image"},
-        ]
+        ],
       },
     }),
   ],
-});
+  description:
+    'Text columns will be disabled if you highlight more than one paragraph of text. Remove the paragraph space, select the whole text block and the text will be allowed to columnise',
+})
