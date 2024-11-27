9db0a056575d16ae65260899171529576e6e317c
eddiewilson <eddie_wilson@hotmail.co.uk>
2024-11-27 13:14:19 +0000

    inline page link fix

--- web/src/components/utils/portableInlineLink.js
+++ web/src/components/utils/portableInlineLink.js
@@ -1,9 +1,8 @@
-import React from 'react'
-import { Link } from '@mui/material';
+import React from "react"
+import { Link } from "@mui/material"
 import { Link as GatsbyLink } from "gatsby-theme-material-ui"
 
 export const PortableTextInlineLink = ({ value, children, color }) => {
-  
   let linkType = ""
   let definedInternal = value?.reference?.slug?.current
   const definedExternal = value?.href
@@ -11,13 +10,16 @@ export const PortableTextInlineLink = ({ value, children, color }) => {
   const isExternal = value?._type === "link"
 
   function checkStringType(str) {
-    const validStrings = ["post", "recipe", "page", "teamMembers"];
-    return validStrings.includes(str);
+    const validStrings = ["post", "recipe", "page", "teamMembers"]
+    return validStrings.includes(str)
   }
 
   const isInternal = checkStringType(value?.reference?._type)
   const isFile = value.reference?._type === "file"
 
+  if (value.reference?._type === "page") {
+    definedInternal = `/${definedInternal}`
+  }
   if (value.reference?._type === "post") {
     definedInternal = `/news/${value?.reference?.categories?.slug?.current}/${definedInternal}`
   }
@@ -28,18 +30,62 @@ export const PortableTextInlineLink = ({ value, children, color }) => {
     definedInternal = `/team-members/${definedInternal}`
   }
   if (isExternal) {
-    linkType = <Link rel={value?.blank && 'noopener'}
-      target={value?.blank && "_blank"} sx={{ a: { "&:hover": { cursor: "pointer" } }, color: color, display: 'inline-block', mx: '5px' }} className="portableTextInlineLink link-animation" href={definedExternal} aria-label={`Link to ${children}`}>{children}</Link>
+    linkType = (
+      <Link
+        rel={value?.blank && "noopener"}
+        target={value?.blank && "_blank"}
+        sx={{
+          a: { "&:hover": { cursor: "pointer" } },
+          color: color,
+          display: "inline-block",
+          mx: "5px",
+        }}
+        className="portableTextInlineLink link-animation"
+        href={definedExternal}
+        aria-label={`Link to ${children}`}
+      >
+        {children}
+      </Link>
+    )
   }
   if (isInternal && !isFile) {
-    linkType = <GatsbyLink sx={{ a: { "&:hover": { cursor: "pointer" } }, color: color, display: 'inline-block', mx: '5px' }} className="portableTextInlineLink link-animation" to={definedInternal} aria-label={`Link to ${children}`}> {children}</GatsbyLink>
+    linkType = (
+      <GatsbyLink
+        sx={{
+          a: { "&:hover": { cursor: "pointer" } },
+          color: color,
+          display: "inline-block",
+          mx: "5px",
+        }}
+        className="portableTextInlineLink link-animation"
+        to={definedInternal}
+        aria-label={`Link to ${children}`}
+      >
+        {" "}
+        {children}
+      </GatsbyLink>
+    )
   }
   if (isFile) {
-    linkType = <Link rel='noopener'
-      target="_blank" sx={{ a: { "&:hover": { cursor: "pointer" } }, color: color, display: 'inline-block', mx: '5px' }} className="portableTextInlineLink link-animation" href={definedExternal} aria-label={`Link to ${children}`}> {children}</Link>
+    linkType = (
+      <Link
+        rel="noopener"
+        target="_blank"
+        sx={{
+          a: { "&:hover": { cursor: "pointer" } },
+          color: color,
+          display: "inline-block",
+          mx: "5px",
+        }}
+        className="portableTextInlineLink link-animation"
+        href={definedExternal}
+        aria-label={`Link to ${children}`}
+      >
+        {" "}
+        {children}
+      </Link>
+    )
   }
 
-  return (
-    linkType
-  )
+  return linkType
 }
