293ae293f662ba68972d0789fbad66c35e9a2fab
eddiewilson <eddie_wilson@hotmail.co.uk>
2024-12-09 13:42:59 +0000

    redirects

--- studio/schemas/redirects.jsx
+++ studio/schemas/redirects.jsx
@@ -1,4 +1,5 @@
 import {defineField, defineType} from 'sanity'
+import {format, parseISO} from 'date-fns'
 
 // Shared validation for our redirect slugs
 const slugValidator = (rule) =>
@@ -14,7 +15,6 @@ export default defineType({
   name: 'redirects',
   title: 'Redirects',
   type: 'document',
-  description: 'Redirect for gatsby-node.js',
   fields: [
     defineField({
       name: 'source',
@@ -30,9 +30,32 @@ export default defineType({
       name: 'permanent',
       type: 'boolean',
     }),
+    defineField({
+      name: 'date',
+      title: 'Date',
+      type: 'datetime',
+      initialValue: () => new Date().toISOString(),
+    }),
   ],
+  description: `Redirect for gatsby-node.js. If you won’t have time to get all the old urls and you decide that any other url that uses your old path /recipes/* should just be redirected to the new /recipe path. Here’s how you’d handle that:
+    source: /recipes/*
+    destination: /recipes
+  `,
   // null / false makes it temporary (307)
   initialValue: {
     permanent: true,
+  },
+  preview: {
+    select: {
+      source: 'source.current',
+      destination: 'destination.current',
+      date: 'date',
+    },
+    prepare({source, destination, date}) {
+      return {
+        title: `Redirect from ${source} to ${destination}`,
+        subtitle: date && `on ${format(parseISO(date), 'LLL d, yyyy')}`,
+      }
+    },
   },
 })
