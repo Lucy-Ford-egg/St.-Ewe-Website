84f767fde3ebb88830997fc241f0abcf2b086357
eddiewilson <eddie_wilson@hotmail.co.uk>
2025-02-04 14:12:07 +0000

    move the api key to the body again

--- web/src/api/klaviyoSubscribe.js
+++ web/src/api/klaviyoSubscribe.js
@@ -46,14 +46,14 @@ export default async function handler(req, res) {
 
     console.log(`List ID = ${process.env.GATSBY_KLAVIYO_LIST_ID}`)
     const response = await fetch(
-      `https://a.klaviyo.com/api/v2/list/${process.env.GATSBY_KLAVIYO_LIST_ID}/subscribe?api_key=${process.env.GATSBY_KLAVIYO_KEY}`,
-
+      `https://a.klaviyo.com/api/v2/list/${process.env.GATSBY_KLAVIYO_LIST_ID}/subscribe`,
       {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
+          api_key: process.env.GATSBY_KLAVIYO_KEY, // Use a secure private key
           profiles: [
             {
               email,
